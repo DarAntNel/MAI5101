be39037e3325b4872510d93687fbce3a29b5713c
Daryl Nelson <darantnel@gmail.com>
2025-03-10 18:35:28 -0400

    added chanegs done to test functions in maing

---
+++ main.py
@@ -0,0 +1,17 @@
+from LAB2.lab2 import factorial
+from LAB2.lab2 import fibonacci
+
+
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    x = factorial(4)
+
+    b = fibonacci(8)
+    print(b)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
