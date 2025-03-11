3d78630c4c19bc85b40e3d76a5aa7b392538f3b0
Daryl Nelson <darantnel@gmail.com>
2025-03-10 22:49:56 -0400

    added implementtion of binary search and merge sort aswell as main.py changes for testing

--- LAB2/lab2.py
+++ LAB2/lab2.py
@@ -11,17 +11,54 @@ def fibonacci(number, number_sequence = [0,1]):
 
     if number == 0:
         return 0
-    if number == 1:
-        return number_sequence[0]
-    if number == 2:
-        return number_sequence[1]
 
-    if len(number_sequence) == number:
+    if number == 1 or number == 2 or len(number_sequence) == number:
         return number_sequence[number-1]
 
     new_number = number_sequence[last_number] + number_sequence[last_number - 1]
     number_sequence.append(new_number)
 
     return fibonacci(number, number_sequence)
+
+
+def binary_search(arr, target, array_start, array_end):
+    if array_end >= array_start:
+
+        array_middle = array_start + (array_end - array_start) // 2
+
+        if arr[array_middle] == target:
+            return array_middle
+        elif arr[array_middle] > target:
+            return binary_search(arr, target, array_start, array_middle - 1)
+        else:
+            return binary_search(arr, target, array_middle + 1, array_end)
+
+    else:
+        return -1
+
+
+def  merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    middle = len(arr) // 2
+
+    left_half = merge_sort(arr[:middle])
+    right_half = merge_sort(arr[middle:])
+
+    return merge(left_half, right_half)
+
+
+def merge(left, right):
+    if not left:
+        return right
+    if not right:
+        return left
+
+    if left[0] < right[0]:
+        return [left[0]] + merge(left[1:], right)
+    else:
+        return [right[0]] + merge(left, right[1:])
+
 
 
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from LAB2.lab2 import factorial
 from LAB2.lab2 import fibonacci
+from LAB2.lab2 import binary_search
+from LAB2.lab2 import merge_sort
 
 
 
@@ -9,9 +11,22 @@ from LAB2.lab2 import fibonacci
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    x = factorial(4)
+    x = factorial(80)
+    print(x)
 
-    b = fibonacci(8)
+    b = fibonacci(2)
     print(b)
 
+    arr = [2, 3, 4, 10, 40]
+    target = 10
+    print(binary_search(arr, target, 0, len(arr) - 1))
+
+    arr = [38, 27, 43, 3, 9, 82, 10]
+    print(merge_sort(arr))
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# 1. What is the time complexity of your Fibonacci function?
+# 2. How does recursion compare to iteration in these cases?
